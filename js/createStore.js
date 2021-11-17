1be83b33743239246563f9aaa30f488988920e7b
danainjax <danainjax@gmail.com>
2021-11-17 06:17:06 -0500

    Done.

--- js/createStore.js
+++ js/createStore.js
@@ -1,28 +1,40 @@
-let state;
+function createStore(reducer) {
+  let state
 
-function reducer(state = { count: 0 }, action) {
+  function dispatch(action) {
+    state = reducer(state, action)
+    render()
+  }
+
+  function getState() {
+    return state
+  }
+
+  return {
+    dispatch,
+    getState,
+  }
+}
+
+function reducer(state = { menu: 'tacos' }, action) {
   switch (action.type) {
-    case 'INCREASE_COUNT':
-      return { count: state.count + 1 };
+    case 'INCREASE_MENU':
+      return { menu: state.menu + ' , more tacos, &' }
 
     default:
-      return state;
+      return state
   }
-};
-
-function dispatch(action){
-  state = reducer(state, action);
-  render();
-};
+}
 
 function render() {
-  let container = document.getElementById('container');
-  container.textContent = state.count;
-};
+  let container = document.getElementById('container')
+  container.textContent = store.getState().menu
+}
 
-dispatch({ type: '@@INIT' })
-let button = document.getElementById('button');
+let store = createStore(reducer) // createStore takes the reducer as an argument
+store.dispatch({ type: '@@INIT' })
+let button = document.getElementById('button')
 
-button.addEventListener('click', function() {
-    dispatch({ type: 'INCREASE_COUNT' });
+button.addEventListener('click', () => {
+  store.dispatch({ type: 'INCREASE_MENU' })
 })
